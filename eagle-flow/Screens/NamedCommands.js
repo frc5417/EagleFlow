4f33aa4a044583d3b0832654941afb30984ed957
Mukesh-Kalikaya <mkalikay598@gmail.com>
2024-04-03 19:26:30 -0500

    Minor changes to the Menu for it to function again

--- eagle-flow/Screens/NamedCommands.js
+++ eagle-flow/Screens/NamedCommands.js
@@ -5,38 +5,32 @@ import { LinearGradient } from "expo-linear-gradient";
 import { NamedStyles } from "../Styles/NamedCommandStyes";
 import Menu from "../Components/Menu";
 
-export default class Named extends Component {
-    constructor(props) {
-        super(props);
-    }
+export default function Named({navigation}){
+    return (
+        <LinearGradient
+            colors={["#00174a", "#2c0c3f", "#3e0031", "#470020", "#48000e"]}
+            start = {{x: 0, y: 0}}
+            end = {{x: 0.5, y: 0.5}}
+            style = {NamedStyles.container}
+        >
+            <Menu navigation = {navigation} />
+            <View style = {NamedStyles.namedCommandsContainer}>
+                <Text style = {NamedStyles.namedCommandsText}> Named Commands </Text>
 
-    render() {
-        return (
-            <LinearGradient
-                colors={["#00174a", "#2c0c3f", "#3e0031", "#470020", "#48000e"]}
-                start = {{x: 0, y: 0}}
-                end = {{x: 0.5, y: 0.5}}
-                style = {NamedStyles.container}
-            >
-                <Menu />
-                <View style = {NamedStyles.namedCommandsContainer}>
-                    <Text style = {NamedStyles.namedCommandsText}> Named Commands </Text>
-
-                    <View style = {NamedStyles.intakeNoteContainer}>
-                        <Text style = {NamedStyles.intakeNoteText}> Intake Note </Text>
-                    </View>
-
-                    <View style = {NamedStyles.shootAngleContainer}>
-                        <Text style = {NamedStyles.shootAngleText}> Shooter At Angle </Text>
-                    </View>
-
-                    <View style = {NamedStyles.elevatorContainer}>
-                        <Text style = {NamedStyles.elevatorText}> Elevator Up</Text>
-                    </View>
-
-                    
+                <View style = {NamedStyles.intakeNoteContainer}>
+                    <Text style = {NamedStyles.intakeNoteText}> Intake Note </Text>
                 </View>
-            </LinearGradient>
-        );
-    }
+
+                <View style = {NamedStyles.shootAngleContainer}>
+                    <Text style = {NamedStyles.shootAngleText}> Shooter At Angle </Text>
+                </View>
+
+                <View style = {NamedStyles.elevatorContainer}>
+                    <Text style = {NamedStyles.elevatorText}> Elevator Up</Text>
+                </View>
+
+                
+            </View>
+        </LinearGradient>
+    );
 }
--- eagle-flow/Styles/AutonGroupsStyles.jsx
+++ eagle-flow/Styles/AutonGroupsStyles.jsx
@@ -29,17 +29,9 @@ export const AutonStyles = StyleSheet.create({
         height: screenHeight / 1.275,
         backgroundColor: 'rgba(59, 105, 224, 0.8)',
         alignContent: 'center',
+        textAlign: 'center',
         alignSelf: 'center',
         borderRadius: 35,
-    },
-
-    mainText: {
-        fontFamily: 'K2D-SemiBold',
-        fontSize: 25 * fontScaleFactor,
-        alignSelf: 'center',
-        color: 'black',
-        textDecorationLine: 'underline',
-        textAlign: "center"
     },
 
     redAutos: {
@@ -49,8 +41,20 @@ export const AutonStyles = StyleSheet.create({
         height: screenHeight / 1.275,
         backgroundColor: 'rgba(204, 53, 65, 0.8)',
         alignContent: 'center',
+        textAlign: 'center',
         alignSelf: 'center',
         borderRadius: 35,
+    },
+
+    mainText: {
+        flex: 0,
+        fontFamily: 'K2D-SemiBold',
+        fontSize: 25 * fontScaleFactor,
+        alignSelf: 'center',
+        color: 'black',
+        textDecorationLine: 'underline',
+        textAlign: 'center',
+        marginLeft: screenWidth / 25,
     },
     
     newAutos: {
@@ -62,6 +66,7 @@ export const AutonStyles = StyleSheet.create({
         alignSelf: 'center',
         borderRadius: 35,
     },
+
     button: {
         backgroundColor: 'rgba(217, 217, 217, .2)',
         borderRadius: 25,
@@ -69,12 +74,14 @@ export const AutonStyles = StyleSheet.create({
         margin: screenHeight / 75,
         padding: 10,
     },
+
     pressHereText: {
         fontFamily: 'K2D-SemiBold',
         fontSize: 10 * fontScaleFactor,
         alignSelf: 'center',
         color: 'white',
     },
+
     blueAutosSmaller: {
         marginLeft: screenWidth / 20,
         marginVertical: screenHeight / 100,
@@ -85,6 +92,7 @@ export const AutonStyles = StyleSheet.create({
         alignSelf: 'center',
         borderRadius: 35,
     },
+
     newAutosSmaller: {
         marginLeft: screenWidth / 20,
         marginVertical: screenHeight / 100,
@@ -95,6 +103,7 @@ export const AutonStyles = StyleSheet.create({
         alignSelf: 'center',
         borderRadius: 35,
     },
+
     redAutosSmaller: {
         marginLeft: screenWidth / 20,
         marginVertical: screenHeight / 100,
@@ -105,12 +114,14 @@ export const AutonStyles = StyleSheet.create({
         alignSelf: 'center',
         borderRadius: 35,
     },
+
     mainTextSmall: {
         fontFamily: 'K2D-SemiBold',
         fontSize: 10 * fontScaleFactor,
         color: 'black',
         alignSelf: 'center',
     },
+
     blueAutosSmallest: {
         display: "flex",
         flexDirection: "row",
@@ -124,6 +135,7 @@ export const AutonStyles = StyleSheet.create({
         paddingHorizontal: 5,
         justifyContent: "space-between"
     },
+
     redAutosSmallest: {
         display: "flex",
         flexDirection: "row",
@@ -137,6 +149,7 @@ export const AutonStyles = StyleSheet.create({
         paddingHorizontal: 5,
         justifyContent: "space-between"
     },
+
     newAutosSmallest: {
         display: "flex",
         flexDirection: "row",
@@ -150,28 +163,33 @@ export const AutonStyles = StyleSheet.create({
         paddingHorizontal: 5,
         justifyContent: "space-between"
     },
+
     image: {
         width: screenWidth * 0.015,
         height: screenWidth * 0.015,
         alignSelf: 'center',
     },
+
     threeDotsImageSmallest: {
         width: screenWidth * 0.015,
         height: screenWidth * 0.015,
         alignSelf: 'center',
     },
+
     threeDotsImage: {
         width: screenWidth * 0.025,
         height: screenWidth * 0.025,
-        alignSelf: "flex-end",
-        marginBottom: screenHeight/200,
-        marginRight: screenWidth/200
+        alignSelf: 'flex-end',
+        marginBottom: screenHeight / 200,
+        marginRight: screenWidth / 200
     },
+
     textHolder: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
     },
+
     textContainer: {
         flex: 1,
     },
